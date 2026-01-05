81ed6504603ee703f3482e3234248bbd8b203204
Liem Dam-Quang <liem.dam-quang@mail.mcgill.ca>
2026-01-05 09:31:28 -0800

    Fix mutex lock implementation to prevent deadlocks if an exception is thrown by tlsf_resource::do_allocate.

--- src/synchronized_tlsf_resource.cpp
+++ src/synchronized_tlsf_resource.cpp
@@ -5,18 +5,16 @@ namespace tlsf {
 void* synchronized_tlsf_resource::do_allocate(std::size_t bytes, std::size_t align) {
     //if align is smaller than block alignment, any allocation 
     //will already be aligned with the desired alignment. 
-    this->mutex.lock();
+    std::scoped_lock<std::mutex> lock(mutex);
     void* ptr = tlsf_resource::do_allocate(bytes, align);
-    this->mutex.unlock();
     return ptr;
 }
 
 void synchronized_tlsf_resource::do_deallocate(void* p, std::size_t bytes, std::size_t align ){
     //The size to be deallocated is already known in the block, so the byte count and 
     //alignment values are not needed.
-    this->mutex.lock();
+    std::scoped_lock<std::mutex> lock(mutex);
     tlsf_resource::do_deallocate(p, bytes, align);
-    this->mutex.unlock();
 }
 
 
